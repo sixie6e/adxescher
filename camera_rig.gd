164ddfa48afb497fde37f83353a1f8d911c0e669
Brent Elisens <37149480+sixie6e@users.noreply.github.com>
2026-08-11 14:44:14 -0400

    Update camera_rig.gd

--- camera_rig.gd
+++ camera_rig.gd
@@ -5,12 +5,12 @@ extends Node3D
 @export var rotation_speed: float = 4.0
 
 func _physics_process(delta: float) -> void:
-		if target_node:
-				global_position = global_position.lerp(target_node.global_position, follow_speed * delta)
-				
-				# slerp rotation to handle instant 90-degree player snaps
-				var target_quat = target_node.global_transform.basis.get_rotation_quaternion()
-				var current_quat = global_transform.basis.get_rotation_quaternion()
-				var new_quat = current_quat.slerp(target_quat, rotation_speed * delta)
-				
-				global_transform.basis = Basis(new_quat)
+	if target_node:
+		global_position = global_position.lerp(target_node.global_position, follow_speed * delta)
+		
+		var target_basis = target_node.global_transform.basis.orthonormalized()
+		var target_quat = target_basis.get_rotation_quaternion()
+		var current_quat = global_transform.basis.orthonormalized().get_rotation_quaternion()
+		
+		var new_quat = current_quat.slerp(target_quat, rotation_speed * delta)
+		global_transform.basis = Basis(new_quat)
