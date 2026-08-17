3f09d96661a651e8b12c6c6a2b1698fd476468ac
Brent Elisens <37149480+sixie6e@users.noreply.github.com>
2026-08-17 14:00:20 -0400

    Update intersection.gd

--- intersection.gd
+++ intersection.gd
@@ -1,9 +1,8 @@
 extends Area3D
 
-func _on_body_entered(body: Node3D) -> void:
-		if body is Player:
-				body.current_intersection = self
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
 
-func _on_body_exited(body: Node3D) -> void:
-		if body is Player and body.current_intersection == self:
-				body.current_intersection = null
+func _on_body_entered(body: Node3D) -> void:
+	if body is Player:
+		body.stop_at_intersection(self)
